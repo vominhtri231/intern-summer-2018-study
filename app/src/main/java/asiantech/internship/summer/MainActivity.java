5250b7579129df5e4e0672ec4c55736321ba7254
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-06-13 10:54:39 +0700

    test ci

--- app/src/main/java/asiantech/internship/summer/MainActivity.java
+++ app/src/main/java/asiantech/internship/summer/MainActivity.java
@@ -2,9 +2,11 @@ package asiantech.internship.summer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int maaa = 1;
+    private TextView TEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
