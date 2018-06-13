731f885f8b8d36a973855a688ba3cb312a815dd8
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-06-13 10:47:05 +0700

    test ci

--- app/src/main/java/asiantech/internship/summer/MainActivity.java
+++ app/src/main/java/asiantech/internship/summer/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int maaa = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
