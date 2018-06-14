400f0e2b1b02cc3a36198eecac8d55225f64a61c
Thanh Hoang N <thanh.hoang@asiantech.vn>
2018-06-14 17:50:45 +0700

    edit file mainActivity.java

---
+++ app/src/at-thanhhoang/java/asiantech/internship/summer/MainActivity.java
@@ -0,0 +1,14 @@
+package asiantech.internship.summer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.util.Log.d("hello", "onCreate: ");
+    }
+}
