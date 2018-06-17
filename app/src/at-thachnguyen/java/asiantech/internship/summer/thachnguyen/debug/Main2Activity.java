d14eaa4a9fd5039dbf2a356962733558b36cdb60
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-17 09:28:31 +0700

    begin do exercise

---
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/Main2Activity.java
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.thachnguyen.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import asiantech.internship.summer.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
