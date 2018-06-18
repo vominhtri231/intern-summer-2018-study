51d212c6f61391da7895eaff06dd54fa4b5531e7
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-06-18 10:28:24 +0700

    test fix CI's comment

--- app/src/main/java/asiantech/internship/summer/MainActivity.java
+++ app/src/main/java/asiantech/internship/summer/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int maaa = 1;
-    private TextView TEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
