630198dbebd462bac43f8662e1520f2e8ba7c5f8
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-18 15:26:40 +0700

    update file Main2Activity.java

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/Main2Activity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/Main2Activity.java
@@ -32,20 +32,21 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         int id=view.getId();
+        String mWhiteColor="#FFFFFF";
         switch (id){
             case R.id.ll3:
                 view1.setBackgroundColor(Color.parseColor("#FF35C611"));
-                view2.setBackgroundColor(Color.parseColor("#FFFFFF"));
-                view3.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                view2.setBackgroundColor(Color.parseColor(mWhiteColor));
+                view3.setBackgroundColor(Color.parseColor(mWhiteColor));
                 break;
             case R.id.ll4:
-                view1.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                view1.setBackgroundColor(Color.parseColor(mWhiteColor));
                 view2.setBackgroundColor(Color.parseColor("#FF35C611"));
-                view3.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                view3.setBackgroundColor(Color.parseColor(mWhiteColor));
                 break;
              case R.id.ll5:
-                view1.setBackgroundColor(Color.parseColor("#FFFFFF"));
-                view2.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                view1.setBackgroundColor(Color.parseColor(mWhiteColor));
+                view2.setBackgroundColor(Color.parseColor(mWhiteColor));
                 view3.setBackgroundColor(Color.parseColor("#FF35C611"));
                 break;
         }
