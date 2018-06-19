47cb0b9824efdbf13409ba40366c671fbdd45747
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-19 09:44:34 +0700

    update file Main2Activity.java

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/Main2Activity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/Main2Activity.java
@@ -1,6 +1,7 @@
 package asiantech.internship.summer.thachnguyen.debug;
 
         import android.graphics.Color;
+        import android.support.v4.content.ContextCompat;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
@@ -10,7 +11,7 @@ package asiantech.internship.summer.thachnguyen.debug;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
     private View view1, view2, view3;
-    private LinearLayout ln3, ln4, ln5;
+    private LinearLayout ll3, ll4, ll5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,35 +20,34 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         view2=findViewById(R.id.view2);
         view3=findViewById(R.id.view3);
 
-        ln4=findViewById(R.id.ll4);
-        ln5=findViewById(R.id.ll5);
-        ln3=findViewById(R.id.ll3);
+        ll4=findViewById(R.id.ll4);
+        ll5=findViewById(R.id.ll5);
+        ll3=findViewById(R.id.ll3);
 
-        ln3.setOnClickListener(this);
-        ln4.setOnClickListener(this);
-        ln5.setOnClickListener(this);
+        ll3.setOnClickListener(this);
+        ll4.setOnClickListener(this);
+        ll5.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         int id=view.getId();
-        String mWhiteColor="#FFFFFF";
         switch (id){
             case R.id.ll3:
-                view1.setBackgroundColor(Color.parseColor("#FF35C611"));
-                view2.setBackgroundColor(Color.parseColor(mWhiteColor));
-                view3.setBackgroundColor(Color.parseColor(mWhiteColor));
+                view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorGreen));
+                view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
                 break;
             case R.id.ll4:
-                view1.setBackgroundColor(Color.parseColor(mWhiteColor));
-                view2.setBackgroundColor(Color.parseColor("#FF35C611"));
-                view3.setBackgroundColor(Color.parseColor(mWhiteColor));
+                view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorGreen));
+                view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
                 break;
              case R.id.ll5:
-                view1.setBackgroundColor(Color.parseColor(mWhiteColor));
-                view2.setBackgroundColor(Color.parseColor(mWhiteColor));
-                view3.setBackgroundColor(Color.parseColor("#FF35C611"));
+                 view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                 view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                 view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorGreen));
                 break;
         }
     }
