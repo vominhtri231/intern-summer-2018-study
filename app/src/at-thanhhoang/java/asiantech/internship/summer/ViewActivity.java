163e7e448f2ab0944b4160d448ed3823612b76b6
at-thanhhoang <thanh.hoang@asiantech.vn>
2018-07-03 00:00:52 +0700

    update interface and change attribute by convention

--- app/src/at-thanhhoang/java/asiantech/internship/summer/ViewActivity.java
+++ app/src/at-thanhhoang/java/asiantech/internship/summer/ViewActivity.java
@@ -2,52 +2,57 @@ package asiantech.internship.summer;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class ViewActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private View view1, view2, view3;
-    LinearLayout ll1, ll2, ll3;
+    private View mViewProfile1, mViewProfile2, mViewProfile3;
+    private LinearLayout mLlProfile1, mLlProfile2, mLlProfile3;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        initView();
+        addListener();
+    }
 
-        view1 = findViewById(R.id.view_animation_1);
-        view2 = findViewById(R.id.view_animation_2);
-        view3 = findViewById(R.id.view_animation_3);
+    private void initView() {
+        mViewProfile1 = findViewById(R.id.viewProfile1);
+        mViewProfile2 = findViewById(R.id.viewProfile2);
+        mViewProfile3 = findViewById(R.id.viewProfile3);
 
-        ll1 = findViewById(R.id.ll_item_1);
-        ll2 = findViewById(R.id.ll_item_2);
-        ll3 = findViewById(R.id.ll_item_3);
+        mLlProfile1 = findViewById(R.id.llProfile1);
+        mLlProfile2 = findViewById(R.id.llProfile2);
+        mLlProfile3 = findViewById(R.id.llProfile3);
+    }
 
-        ll1.setOnClickListener(this);
-        ll2.setOnClickListener(this);
-        ll3.setOnClickListener(this);
+    private void addListener() {
+        mLlProfile1.setOnClickListener(this);
+        mLlProfile2.setOnClickListener(this);
+        mLlProfile3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         int id = view.getId();
         switch (id) {
-            case R.id.ll_item_1:
-                view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPressed));
-                view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
-                view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
+            case R.id.llProfile1:
+                mViewProfile1.setBackgroundResource(R.color.colorOrangeDark);
+                mViewProfile2.setBackgroundResource(R.color.colorYellowPale);
+                mViewProfile3.setBackgroundResource(R.color.colorYellowPale);
                 break;
-            case R.id.ll_item_2:
-                view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
-                view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPressed));
-                view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
+            case R.id.llProfile2:
+                mViewProfile1.setBackgroundResource(R.color.colorYellowPale);
+                mViewProfile2.setBackgroundResource(R.color.colorOrangeDark);
+                mViewProfile3.setBackgroundResource(R.color.colorYellowPale);
                 break;
-            case R.id.ll_item_3:
-                view1.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
-                view2.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDefault));
-                view3.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPressed));
+            case R.id.llProfile3:
+                mViewProfile1.setBackgroundResource(R.color.colorYellowPale);
+                mViewProfile2.setBackgroundResource(R.color.colorYellowPale);
+                mViewProfile3.setBackgroundResource(R.color.colorOrangeDark);
                 break;
         }
     }
