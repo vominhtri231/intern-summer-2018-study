579fe15ddef8c616ad8191dd5108f038dc37e2c7
at-trivo <tri.vo@asiantech.vn>
2018-07-03 08:22:39 +0700

    init viewpager

---
+++ app/src/at-trivo/java/asiantech/internship/summer/view_pager/ViewPagerActivity.java
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.view_pager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import asiantech.internship.summer.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+    }
+}
