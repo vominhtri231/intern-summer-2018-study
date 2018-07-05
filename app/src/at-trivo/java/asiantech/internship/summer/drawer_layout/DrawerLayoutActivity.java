cd8b8076174d6952340b1a474f54195b163ef272
at-trivo <tri.vo@asiantech.vn>
2018-07-05 11:13:38 +0700

    first drawer layout commit

---
+++ app/src/at-trivo/java/asiantech/internship/summer/drawer_layout/DrawerLayoutActivity.java
@@ -0,0 +1,17 @@
+package asiantech.internship.summer.drawer_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import asiantech.internship.summer.R;
+
+public class DrawerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e("VVV","1");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_layout);
+    }
+}
