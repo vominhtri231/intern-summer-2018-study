f295c6e47683bea04b44df2f1ae31661bb8e1ba6
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-07-12 10:31:24 +0700

    start do asynctask handler thread

---
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/asynctask_thread_and_handler/AsyncTaskThread_HandlerActivity.java
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.asynctask_thread_and_handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import asiantech.internship.summer.R;
+
+public class AsyncTaskThread_HandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task_thread__handler);
+    }
+}
