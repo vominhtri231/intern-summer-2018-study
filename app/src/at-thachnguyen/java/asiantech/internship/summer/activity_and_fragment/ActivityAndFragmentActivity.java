d16d457b68e55ec5137679ab0fb479d56f2d157a
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-07-16 17:20:12 +0700

    handle back button on toolbar

--- app/src/at-thachnguyen/java/asiantech/internship/summer/activity_and_fragment/ActivityAndFragmentActivity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/activity_and_fragment/ActivityAndFragmentActivity.java
@@ -1,12 +1,15 @@
 package asiantech.internship.summer.activity_and_fragment;
 
+import android.annotation.SuppressLint;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import asiantech.internship.summer.R;
 
+@SuppressLint("Registered")
 public class ActivityAndFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,7 @@ public class ActivityAndFragmentActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Log in");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         LoginFragment mLoginFragment = new LoginFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.fragmentContainer, mLoginFragment);
@@ -30,5 +34,18 @@ public class ActivityAndFragmentActivity extends AppCompatActivity {
         } else if (position > 1) {
             getSupportFragmentManager().popBackStack();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            int position = getSupportFragmentManager().getBackStackEntryCount();
+            if (position == 1) {
+                finish();
+            } else if (position > 1) {
+                getSupportFragmentManager().popBackStack();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/at-thachnguyen/java/asiantech/internship/summer/activity_and_fragment/HomeActivity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/activity_and_fragment/HomeActivity.java
@@ -1,14 +1,17 @@
 package asiantech.internship.summer.activity_and_fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 import asiantech.internship.summer.R;
 
+@SuppressLint("Registered")
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -26,5 +29,13 @@ public class HomeActivity extends AppCompatActivity {
             tvEmail.setText(bundle.getString(LoginFragment.DATA_RECEIVE_EMAIL));
             tvPassword.setText(bundle.getString(LoginFragment.DATA_RECEIVE_PASSWORD));
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
