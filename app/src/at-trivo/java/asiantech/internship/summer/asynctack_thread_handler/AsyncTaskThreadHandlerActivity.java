244adc131b1bd9cf395e38736004b98c048eda86
at-trivo <tri.vo@asiantech.vn>
2018-07-16 10:05:13 +0700

    first commit

---
+++ app/src/at-trivo/java/asiantech/internship/summer/asynctack_thread_handler/AsyncTaskThreadHandlerActivity.java
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.asynctack_thread_handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import asiantech.internship.summer.R;
+
+public class AsyncTaskThreadHandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task_thread_handler);
+    }
+}
