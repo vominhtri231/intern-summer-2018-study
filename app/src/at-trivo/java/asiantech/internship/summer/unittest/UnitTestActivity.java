5b2078f1cd45983ea172ee64eb1311c16797770b
at-trivo <tri.vo@asiantech.vn>
2018-07-30 12:00:05 +0700

    add view

---
+++ app/src/at-trivo/java/asiantech/internship/summer/unittest/UnitTestActivity.java
@@ -0,0 +1,18 @@
+package asiantech.internship.summer.unittest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import asiantech.internship.summer.R;
+
+public class UnitTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_test);
+    }
+}
