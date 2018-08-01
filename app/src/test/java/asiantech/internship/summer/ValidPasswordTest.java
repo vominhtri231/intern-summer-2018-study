4b5eccf7a3858546db6f2741a1b8cb5654292329
at-trivo <tri.vo@asiantech.vn>
2018-08-01 11:58:16 +0700

    remove test line

--- app/src/test/java/asiantech/internship/summer/ValidPasswordTest.java
+++ app/src/test/java/asiantech/internship/summer/ValidPasswordTest.java
@@ -21,49 +21,42 @@ public class ValidPasswordTest {
 
     @Test
     public void passwordEqualsUsername() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("ijsAhdgkaSl");
         assertEquals(Validator.validate(user), R.string.error_username_equals_password);
     }
 
     @Test
     public void passwordRepeatChar() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("ndaewgeerieR");
         assertEquals(Validator.validate(user), R.string.error_password_repeat_char);
     }
 
     @Test
     public void passwordNotHas2SpecialChar() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("nbewgerier");
         assertEquals(Validator.validate(user), R.string.error_password_not_has_2_special_char);
     }
 
     @Test
     public void passwordNotHas3Uppercase() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("n1ewge7rieR");
         assertEquals(Validator.validate(user), R.string.error_password_not_has_3_uppercase);
     }
 
     @Test
     public void passwordHasLessThan8Char() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("n1RGE7R");
         assertEquals(Validator.validate(user), R.string.error_password_has_less_than_8_char);
     }
 
     @Test
     public void passwordEndWithSpecialChar() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("n1RGE7Rgajsd1");
         assertEquals(Validator.validate(user), R.string.error_password_end_with_special_char);
     }
 
     @Test
     public void passwordAndUsernameValid() {
-        Mockito.when(user.getUsername()).thenReturn("ijsAhdgkaSl");
         Mockito.when(user.getPassword()).thenReturn("n1RGE7Rgajsde");
         assertEquals(Validator.validate(user), R.string.valid_username_password);
     }
