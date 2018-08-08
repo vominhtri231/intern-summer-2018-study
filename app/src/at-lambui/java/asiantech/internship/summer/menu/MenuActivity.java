7bd5ad264879fd8d61021565e30ac6df4ff7d129
at-lambui <lam.bui@asiantech.vn>
2018-08-09 00:21:58 +0700

    create UITest.activity, complete excercise

--- app/src/at-lambui/java/asiantech/internship/summer/menu/MenuActivity.java
+++ app/src/at-lambui/java/asiantech/internship/summer/menu/MenuActivity.java
@@ -3,39 +3,67 @@ package asiantech.internship.summer.menu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.activity_fragment.HomeActivity;
 import asiantech.internship.summer.recyclerview.RecyclerViewActivity;
 import asiantech.internship.summer.ViewAndViewGruopActivity;
+import asiantech.internship.summer.unittest.UnitTestActivity;
 import asiantech.internship.summer.viewpager.PagerActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mBtnViewAndViewGroup;
+    Button mBtnFragmentAndListener;
+    Button mBtnRecyclerView;
+    Button mBtnViewPager;
+    Button mBtnUnitTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Button btnRecyclerview = findViewById(R.id.btnRecyclerview);
-
-        Button btn_ExerciseFragment = findViewById(R.id.btnExerciseFragment);
-        btn_ExerciseFragment.setOnClickListener(v -> {
-            Intent intentFragment = new Intent(MenuActivity.this, HomeActivity.class);
-            startActivity(intentFragment);
-        });
-        btnRecyclerview.setOnClickListener(view -> {
-            Intent intentRecyclerview = new Intent(MenuActivity.this, RecyclerViewActivity.class);
-            startActivity(intentRecyclerview);
-        });
-        Button btnViewAndViewGroup = findViewById(R.id.btnViewAndViewGroup);
-        btnViewAndViewGroup.setOnClickListener(view -> {
-            Intent intent = new Intent(MenuActivity.this, ViewAndViewGruopActivity.class);
-            startActivity(intent);
-        });
-        Button btnViewpager = findViewById(R.id.btnViewPager);
-        btnViewpager.setOnClickListener(view -> {
-            Intent intentViewpager = new Intent(MenuActivity.this, PagerActivity.class);
-            startActivity(intentViewpager);
-        });
+        initViews();
+        addListeners();
+    }
+    private void initViews() {
+        mBtnViewAndViewGroup = findViewById(R.id.btnViewAndViewGroup);
+        mBtnFragmentAndListener = findViewById(R.id.btnExerciseFragment);
+        mBtnRecyclerView = findViewById(R.id.btnRecyclerview);
+        mBtnViewPager = findViewById(R.id.btnViewPager);
+        mBtnUnitTest = findViewById(R.id.btnUnitTest);
+    }
+    private void addListeners() {
+        mBtnViewAndViewGroup.setOnClickListener(this);
+        mBtnFragmentAndListener.setOnClickListener(this);
+        mBtnRecyclerView.setOnClickListener(this);
+        mBtnViewPager.setOnClickListener(this);
+        mBtnUnitTest.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btnViewAndViewGroup:
+                intent = new Intent(MenuActivity.this, ViewAndViewGruopActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnExerciseFragment:
+                intent = new Intent(MenuActivity.this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnRecyclerview:
+                intent = new Intent(MenuActivity.this, RecyclerViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnViewPager:
+                intent = new Intent(MenuActivity.this, PagerActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnUnitTest:
+                intent = new Intent(MenuActivity.this, UnitTestActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
