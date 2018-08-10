cbe82f1338013dc220982db632135141032ec7a6
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-08-10 16:43:17 +0700

    fix comment

--- app/src/at-thachnguyen/java/asiantech/internship/summer/canvas/model/Wildlife.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/canvas/model/Wildlife.java
@@ -1,25 +1,25 @@
 package asiantech.internship.summer.canvas.model;
 
 public class Wildlife {
-    private final float Dolphin;
-    private final float Whale;
-    private final int Year;
+    private final float dolphin;
+    private final float whale;
+    private final int year;
 
     public Wildlife(float dolphin, float whale, int year) {
-        Dolphin = dolphin;
-        Whale = whale;
-        Year = year;
+        this.dolphin = dolphin;
+        this.whale = whale;
+        this.year = year;
     }
 
     public float getDolphin() {
-        return Dolphin;
+        return dolphin;
     }
 
     public float getWhale() {
-        return Whale;
+        return dolphin;
     }
 
     public int getYear() {
-        return Year;
+        return year;
     }
 }
